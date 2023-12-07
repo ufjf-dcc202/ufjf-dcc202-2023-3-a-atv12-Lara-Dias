3f03d15f961d1bce6fd94bbf024d73c9f49b98ae
Lara Dias <lara.dias@estudante.ufjf.br>
2023-12-06 21:00:32 -0300

    cria estoque.js main.js e suas respectivas funções e constantes, assim como adiciona label e fieldset no index.html

---
+++ estoque.js
@@ -0,0 +1,14 @@
+//estoque.js
+
+let estoque ={
+    'joao': [{'tipo': "maca", 'qtd': 1},
+            {'tipo': "pera", 'qtd': 1}
+],   
+'maria': [{tipo: "maca", 'qtd': 2},
+        {'tipo': "pera", 'qtd': 4}
+    ]
+};
+
+export function getEstoque(){
+    return estoque;
+}
