0e41bef330eb1a281307d7c896d3f008a317ef6a
Lara Dias <lara.dias@estudante.ufjf.br>
2023-12-13 20:18:38 -0300

    add estoque joao e maria no index.html e nomain.js

--- main.js
+++ main.js
@@ -1,16 +1,18 @@
 //main.js
 
-import { transacao } from "./estoque";
+import { getEstoque, transacao } from "./estoque.js";
 
 document.entrada.addEventListener('submit', leFormulario);
 
 const olJoao = document.querySelector("#olJoao");
 const olMaria = document.querySelector("#olMaria");
 
+atualizaTela();
+
 function leFormulario(event){
     event.preventDefault();
     const fruta = document.entrada.fruta.value;
-    const quantidade = document.entrada.quantidade.value;
+    const quantidade = document.entrada.quantidade.valueAsNumber;
     const origem = document.entrada.origem.value;
     const destino= document.entrada.destino.value;
 
@@ -19,4 +21,21 @@ function leFormulario(event){
     //document.entrada.submit();
 
     transacao(origem, destino, quantidade, fruta);
+    atualizaTela();
+}
+
+function atualizaTela() {
+    const estoque = getEstoque();
+    preencheListaPessoa(estoque['joao'], olJoao);
+    preencheListaPessoa(estoque['maria'], olMaria);
+}
+
+function preencheListaPessoa(pessoa, lista) {
+    lista.innerHTML ="";
+    for(let i = 0; i<pessoa.length; i++){
+        const monte = pessoa[i];
+        const eLi = document.createElement('li');
+        eLi.innerText = `${monte.tipo}: ${monte.qtd}`;
+        lista.append(eLi);
+    }
 }
