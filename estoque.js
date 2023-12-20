b36fee957fbb754a2b27babac6ef1f6abac5480e
Lara Dias <lara.dias@estudante.ufjf.br>
2023-12-19 22:21:29 -0300

    ajuste na função transacaoNoEstoque na estoque.js

--- estoque.js
+++ estoque.js
@@ -1,8 +1,8 @@
 //estoque.js
 
 let estoque ={
-    'joao': [{'tipo': 'maca', 'qtd': 1}],   
-    'maria': [{'tipo': 'maca', 'qtd': 2}]
+    'joao': [{'tipo': 'maca', 'quantidade': 1}],   
+    'maria': [{'tipo': 'maca', 'quantidade': 2}]
 };
 
 export function getEstoque(){
@@ -14,19 +14,65 @@ export function limpaEstoque(){
 }
 
 export function transacaoNoEstoque(origem, destino, quantidade, fruta){
-    if(origem === 'pomar'){
-        const pessoa = estoque[destino];
-        for (let i = 0; i < pessoa.length; i++) {
-            let monte;
-            if(monte.tipo === fruta){
-                monte = pessoa[i];
-                break;
+    if(origem === destino || quantidade <= 0){
+        return;
+    }
+  
+    if(origem !== "pomar" && !estoque[origem]){
+        estoque[origem] = [];
+    }  
+  
+    if(destino !== "pomar" && !estoque[destino]){
+        estoque[destino] = [];
+    }  
+    
+    if(destino === "pomar"){
+        const itemEncontrado = estoque[origem].find(item => item.tipo === tipo);
+    
+        if(itemEncontrado) {
+            itemEncontrado.quantidade = Math.max(0, itemEncontrado.quantidade - quantidade);
+        }
+    
+        return;
+    }  
+
+    if(origem === "pomar"){
+        const itemEncontrado = estoque[destino].find(item => item.tipo === tipo);
+    
+        if(itemEncontrado){
+            itemEncontrado.quantidade += quantidade;
+        } 
+        else{
+            estoque[destino].push({tipo, quantidade});
+        }
+        return;
+    } 
+    else {
+        const itemOrigem = estoque[origem].find(item => item.tipo === tipo);
+        const itemDestino = estoque[destino].find(item => item.tipo === tipo);
+
+        if(!itemOrigem){
+            return;
+        } 
+        else if(quantidade > itemOrigem.quantidade){
+            if(itemDestino){
+                itemDestino.quantidade += itemOrigem.quantidade;
+            } 
+            else{
+                estoque[destino].push({tipo: tipo, quantidade: itemOrigem.quantidade})
             }
-            if(monte === undefined){
-              monte = {'tipo': fruta, 'qtd': 0};
-              pessoa.push(monte);
+            itemOrigem.quantidade = 0
+        } 
+        else {
+            if(itemDestino){
+                itemDestino.quantidade += quantidade;
+            } 
+            else {
+                estoque[destino].push({tipo, quantidade});
             }
-            monte.qtd += quantidade;
+            itemOrigem.quantidade -= quantidade;
         }
     }
+    
+    return;
 }
